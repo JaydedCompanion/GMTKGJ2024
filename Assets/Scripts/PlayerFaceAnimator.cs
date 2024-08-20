ba340d4babd39442f5aa75b94028ad2461433883
Juan Callejas <jcallejasrodriguez@gmail.com>
2024-08-20 12:48:22 -0400

    Scene transitions, tutorials, etc, IT'S GO TIME I DON'T HAVE TIME FOR COMMIT MESSAGES

--- Assets/Scripts/PlayerFaceAnimator.cs
+++ Assets/Scripts/PlayerFaceAnimator.cs
@@ -10,11 +10,13 @@ public class PlayerFaceAnimator : MonoBehaviour {
     public Sprite blink;
     public Sprite smile;
     public Sprite grimace;
-    public Sprite pleading;
+    public Sprite [] easterEggs;
     public float minBlinkTime;
     public float maxBlinkTime;
     public float blinkDuration;
     public float blinkTimer;
+    public float idleDuration;
+    public float idleTimer;
 
     private Transform estrangedParent;
     private SpriteRenderer renderer;
@@ -26,18 +28,35 @@ public class PlayerFaceAnimator : MonoBehaviour {
         estrangedParent = transform.parent;
         transform.parent = null;
         renderer = GetComponent<SpriteRenderer> ();
-
+        idleTimer = idleDuration;
     }
 
     // Update is called once per frame
     void Update () {
 
         blinkTimer -= Time.deltaTime;
+        idleTimer -= Time.deltaTime;
+
+        if (
+            Mathf.Abs (Input.GetAxisRaw ("Horizontal")) > 0.5f ||
+            Mathf.Abs (Input.GetAxisRaw ("Vertical")) > 0.5f ||
+            Input.GetButton ("Submit") ||
+            Input.GetButton ("Jump")
+            )
+            idleTimer = idleDuration;
+
+        if (idleTimer < 0 && idleTimer > -1) {
+            blinkTimer = blinkDuration;
+            idleTimer = -1;
+        }
+
         if (blinkTimer <= blinkDuration) {
             renderer.sprite = blink;
             if (blinkTimer < 0) {
                 blinkTimer = Random.Range (minBlinkTime, maxBlinkTime);
                 renderer.sprite = idle;
+                if (idleTimer < 0)
+                    renderer.sprite = easterEggs [Random.Range (0, easterEggs.Length)];
             }
         } else {
             if (Mathf.Abs (Input.GetAxisRaw ("Horizontal")) > 0.5f && !Resizer.instance.holdingPlayer) {
